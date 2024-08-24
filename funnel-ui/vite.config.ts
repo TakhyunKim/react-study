826f3cffb7bbdf8aaa0acd703b0fafd52e511037
TakhyunKim <kimkih1218@gmail.com>
2024-08-24 15:00:06 +0900

    chore: apply absolute path setting

--- funnel-ui/vite.config.ts
+++ funnel-ui/vite.config.ts
@@ -1,7 +1,18 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: __dirname,
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+      {
+        find: "@/pages",
+        replacement: path.resolve(__dirname, "src/pages"),
+      },
+    ],
+  },
 });
